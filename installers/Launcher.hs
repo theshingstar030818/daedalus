316af23c1eb7d409efee9bc6d2f0f2256f6b127c
Ilya Peresadin <pva701@gmail.com>
2017-12-01 16:13:13 +0300

    [CSL-1984] Prepare installers for master

--- installers/Launcher.hs
+++ installers/Launcher.hs
@@ -54,11 +54,11 @@ launcherArgs Launcher{..} = unwords $
               ]
       configurationArgs | os == "mingw32" =
                           [ "--configuration-file", quote "%DAEDALUS_DIR%\\configuration.yaml"
-                          , "--configuration-key",  quote "mainnet_wallet_win64"
+                          , "--configuration-key",  quote "dev"
                           ]
                         | otherwise =
                           [ "--configuration-file", quote "./configuration.yaml"
-                          , "--configuration-key",  quote "mainnet_wallet_macos64"
+                          , "--configuration-key",  quote "dev"
                          ]
       nodeDbPath = runtimePath <> "DB-" <> version
       nodeArgs = [
